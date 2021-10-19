daa174e3c464d39549afcadd342bb4bbb0e8e87f
malcodeman <26983729+malcodeman@users.noreply.github.com>
2021-10-19 20:22:22 +0200

    Adds notion

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
-console.log("hello");
+import { Client } from "@notionhq/client";
+
+const notion = new Client({
+  auth: process.env.NOTION_TOKEN,
+});
+
+async function main() {
+  const users = await notion.databases.query({
+    database_id: "c5aba6d2ffb741d7a02a65919818e555",
+  });
+
+  console.log("users", users.results);
+}
+
+main();
